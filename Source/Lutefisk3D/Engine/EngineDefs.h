b0b4e99e342219368840078d28f88e7684dee21a
nemerle <nemerle5@gmail.com>
2017-04-04 17:14:15 +0200

    More missing files...

---
+++ Source/Lutefisk3D/Engine/EngineDefs.h
@@ -0,0 +1,77 @@
+//
+// Copyright (c) 2008-2017 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+#include <QtCore/QString>
+namespace Urho3D
+{
+
+// Engine parameters
+static const QString EP_AUTOLOAD_PATHS("AutoloadPaths");
+static const QString EP_BORDERLESS("Borderless");
+static const QString EP_DUMP_SHADERS("DumpShaders");
+static const QString EP_EVENT_PROFILER("EventProfiler");
+static const QString EP_EXTERNAL_WINDOW("ExternalWindow");
+static const QString EP_FLUSH_GPU("FlushGPU");
+static const QString EP_FORCE_GL2("ForceGL2");
+static const QString EP_FRAME_LIMITER("FrameLimiter");
+static const QString EP_FULL_SCREEN("FullScreen");
+static const QString EP_HEADLESS("Headless");
+static const QString EP_HIGH_DPI("HighDPI");
+static const QString EP_LOG_LEVEL("LogLevel");
+static const QString EP_LOG_NAME("LogName");
+static const QString EP_LOG_QUIET("LogQuiet");
+static const QString EP_LOW_QUALITY_SHADOWS("LowQualityShadows");
+static const QString EP_MATERIAL_QUALITY("MaterialQuality");
+static const QString EP_MONITOR("Monitor");
+static const QString EP_MULTI_SAMPLE("MultiSample");
+static const QString EP_ORIENTATIONS("Orientations");
+static const QString EP_PACKAGE_CACHE_DIR("PackageCacheDir");
+static const QString EP_RENDER_PATH("RenderPath");
+static const QString EP_REFRESH_RATE("RefreshRate");
+static const QString EP_RESOURCE_PACKAGES("ResourcePackages");
+static const QString EP_RESOURCE_PATHS("ResourcePaths");
+static const QString EP_RESOURCE_PREFIX_PATHS("ResourcePrefixPaths");
+static const QString EP_SHADER_CACHE_DIR("ShaderCacheDir");
+static const QString EP_SHADOWS("Shadows");
+static const QString EP_SOUND("Sound");
+static const QString EP_SOUND_BUFFER("SoundBuffer");
+static const QString EP_SOUND_INTERPOLATION("SoundInterpolation");
+static const QString EP_SOUND_MIX_RATE("SoundMixRate");
+static const QString EP_SOUND_STEREO("SoundStereo");
+static const QString EP_TEXTURE_ANISOTROPY("TextureAnisotropy");
+static const QString EP_TEXTURE_FILTER_MODE("TextureFilterMode");
+static const QString EP_TEXTURE_QUALITY("TextureQuality");
+static const QString EP_TIME_OUT("TimeOut");
+static const QString EP_TOUCH_EMULATION("TouchEmulation");
+static const QString EP_TRIPLE_BUFFER("TripleBuffer");
+static const QString EP_VSYNC("VSync");
+static const QString EP_WINDOW_HEIGHT("WindowHeight");
+static const QString EP_WINDOW_ICON("WindowIcon");
+static const QString EP_WINDOW_POSITION_X("WindowPositionX");
+static const QString EP_WINDOW_POSITION_Y("WindowPositionY");
+static const QString EP_WINDOW_RESIZABLE("WindowResizable");
+static const QString EP_WINDOW_TITLE("WindowTitle");
+static const QString EP_WINDOW_WIDTH("WindowWidth");
+static const QString EP_WORKER_THREADS("WorkerThreads");
+
+}
